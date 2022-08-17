76b418d5ea2d7853d055e126ef55e7bce373e8c7
invtr07 <95737990+invtr07@users.noreply.github.com>
2022-08-17 14:20:45 +0600

    basic MERN app setup

---
+++ Backend/src/server.js
@@ -0,0 +1,35 @@
+//boilerplate code of connecting server with mongodb - from OutSite
+
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const PORT = process.env.PORT ||  8000
+const URI = process.env.OUTSITE_DB_URI
+const app = express();
+
+//using middleware
+app.use(express.json());
+app.use(cors());
+
+//import the routes
+import userRoutes from './routes/user.js'
+
+//using routes
+app.use("/api", userRoutes)
+
+//db connection establishing
+mongoose.connect(URI)
+    .then(()=>{
+        console.log("DB is connected")
+    })
+    .catch(()=>{
+        console.log("Unable to connect to DB")
+    })
+
+//running server
+app.listen(PORT, ()=>{
+    console.log(`App is running on port ${PORT}`)
+})
