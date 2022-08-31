29631395f29317c116c135ad636a305a7d1007dd
invtr07 <95737990+invtr07@users.noreply.github.com>
2022-08-31 15:38:08 +0600

    finally made copy to clipboard

--- Frontend/src/pages/Savings/SavedCard.jsx
+++ Frontend/src/pages/Savings/SavedCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import './SavedCard.css'
 
@@ -33,7 +34,11 @@ export default function SavedCard() {
      ])
      const[confirm, setConfirm]=React.useState(false) //hook that makes Close modal appear
      const[data, setData]=React.useState() //hook for entering data about chosen card into 
+     const[copy, setCopy]=React.useState(false) //hook for making a copy of the text to clipboard
+     const[copyModal,setcopyModal]=React.useState(false)
+     const[copyName, setCopyName]=React.useState("")
 
+     //function that deletes item from an array of cards
      const handleDelete =({key})=>{
           const itemIndex = cards.findIndex((item)=> item.key ===key);
           const leftSideofArray = cards.slice(0, itemIndex);
@@ -44,17 +49,41 @@ export default function SavedCard() {
      }
 
      //confirm before delete modal 
-
      const closeDeleteModal =()=>setConfirm(false)
-
      const setDeleteModal =(data)=>{
           setConfirm(true);
           setData(data);
      }
 
-     let modal
+
+     const handleCopyModal =(name)=>{
+               setCopy(true)
+               setCopyName(name)
+     }
+
+     React.useEffect(()=>{
+          if(copyName.length!==0){
+               console.log(copyName)
+               setcopyModal(true)
+          }
+          else{
+               console.log("not copied")
+          }
+          
+     },[copyName])
+
+     let SuccessCopyModal
+     if(copyModal){
+          SuccessCopyModal=<Dialog open={copyModal} onClose={()=>{setcopyModal(false)}}>
+                                   <DialogContent sx={{fontFamily:"'Roboto', sans-serif"}}>
+                                        Текст файла <b>"{copyName}"</b> был скопирован
+                                   </DialogContent>
+          </Dialog>
+     }
+
+     let deleteModal
      if(confirm){
-          modal= <Dialog sx={{borderRadius:"30px"}}open={confirm} onClose={closeDeleteModal}>
+          deleteModal= <Dialog sx={{borderRadius:"30px"}}open={confirm} onClose={closeDeleteModal}>
                          <DialogTitle alignSelf={"center"}>Delete File</DialogTitle>
                          <DialogContent sx={{fontFamily:"'Roboto', sans-serif", width:"80%"}}>Are you sure you want to delete "{data.FileName}" file</DialogContent>
                          <DialogActions>
@@ -62,12 +91,13 @@ export default function SavedCard() {
                               <Button variant="contained" onClick={()=>{handleDelete(data)}}>Confirm</Button>
                          </DialogActions>
                     </Dialog>
-     }
-          
+     }    
+
 
   return (
     <>
-          {modal}
+          {SuccessCopyModal}
+          {deleteModal}
           <div className='cards'>
                {cards.map((data)=>
                     <Card sx={{boxShadow:"0px 0px 5px 0px grey"}}key={data.key}>
@@ -80,14 +110,17 @@ export default function SavedCard() {
                                    {data.PreviewText}...
                                    </Typography>
                               </CardContent>
-             </CardActionArea>
-                         <Divider/>
-                         <CardActions className='action-box'>
-                              <div style={{fontSize:"15px"}}>{data.createdTime}</div>
-                              <div className='icons'>
-                                   <div className='copy-icon'></div>
+                         </CardActionArea>
+                              <Divider/>
+                              <CardActions className='action-box'>
+                                   <div style={{fontSize:"15px"}}>{data.createdTime}</div>
+                                   <div className='icons'>
+                                        <CopyToClipboard text={data.PreviewText} onCopy={()=>{handleCopyModal(data.FileName)}}>
+                                             <div className='copy-icon'></div>
+                                        </CopyToClipboard>
+                                        
                                    <div onClick={()=>(setDeleteModal(data))}className='delete-icon'></div>
-                              </div>    
+                                   </div>    
                          </CardActions>
                     </Card>
                )}
