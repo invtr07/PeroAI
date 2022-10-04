1441e9881756f2eeeb298d2f3f1d0f0fdd7c4318
invtr07 <95737990+invtr07@users.noreply.github.com>
2022-10-04 11:31:22 +0100

    branch test

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,25 +1,42 @@
-//boilerplate code of connecting server with mongodb - from OutSite
-
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import {pg as Client} from 'pg'
 dotenv.config();
 
 const PORT = process.env.PORT 
-
 const app = express();
 
-//using middleware
+//middleware
 app.use(express.json());
 app.use(cors());
 
-//import the routes
-import userRoutes from './routes/user.js'
+import authorization  from './routes/authorization.js'
 
-//using routes
-app.use("/api", userRoutes)
+// app.post('/api/result', (req, res)=>{
+//     res.send("works")
+// })
 
-//running server
-app.listen(PORT, ()=>{
-    console.log(`App is running on port ${PORT}`)
-})
+async function DbConnect(){
+    try{
+        const client = new Client({
+            host: "localhost",
+            user: process.env.DB_USER,
+            database: process.env.DATABASE,
+            port: process.env.DB_PORT,
+            password: process.env.PASSWORD
+        })
+        await client.connect()
+        app.listen(PORT, ()=>{
+            console.log(`App is running on port ${PORT}`)
+        })
+        console.log("Db is Connected")
+    }
+    catch{
+        console.log("error")
+    }
+}
+
+DbConnect()
+
+
